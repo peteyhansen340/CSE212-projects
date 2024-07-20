bce82826813228d9b180379b994a9623668d07cb
whansen340 <peteyhansen340@gmail.com>
2024-07-20 13:36:07 -0600

    tree

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -15,7 +15,7 @@ public class Node {
             else
                 Left.Insert(value);
         }
-        else {
+        else if (value > Data) {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
@@ -26,11 +26,18 @@ public class Node {
 
     public bool Contains(int value) {
         // TODO Start Problem 2
-        return false;
+        if (value == Data) {
+            return true;
+        } else if (value < Data) {
+            return Left != null && Left.Contains(value);
+        } else {
+            return Right != null && Right.Contains(value);
+        }
     }
 
     public int GetHeight() {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = (Left!= null) ? Left.GetHeight() : 0;
+        int rightHeight = (Right!= null) ? Right.GetHeight() : 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
